1db5753e22eea3108d8dca0638826714fe0af23b
Gitanjali Poddar (ETHZ) [gpoddar] <gpoddar@t3ui02.psi.ch>
2018-10-17 14:20:27 +0200

    angles

--- GenResponse/python/hhgen/utils.py
+++ GenResponse/python/hhgen/utils.py
@@ -13,24 +13,21 @@ def calc_p4extra(df,prefix):
     df[prefix+"_m"] = np.sqrt( en**2 - px**2 -py**2 -pz**2 )
 
 ## -----------------------------------------------------------------------------------------
-def calc_sump4(df,dest,part1,part2):
+def calc_sump4(df,dest,part1,part2): 
     for comp in "_px","_py","_pz","_e":
         df[dest+comp] = df[part1+comp] + df[part2+comp]
     calc_p4extra(df,dest)
 
 ## -----------------------------------------------------------------------------------------
-def cal_cos_theta_hlx(df):
-    pass
+def calc_cos_theta(df, part):
+    df["cos_theta_"+part] = np.cos(2*np.arctan(np.exp(-df["h"+part+"_eta"])))
 
 ## -----------------------------------------------------------------------------------------
-def calc_cos_theta_cs(diPho,diJet):
-    pass
+def calc_cos_theta_cs(df):
+    num=np.abs(np.sinh(df["hh_delta_eta"]))*2.*df["hgg_pt"]*df["hbb_pt"]
+    den=np.sqrt(1+(df["hh_pt"]/df["hh_m"])**2.)*(df["hh_m"]**2)
+    df["cos_theta_cs"]=num/den
+    
 
-## -----------------------------------------------------------------------------------------
-def cos_theta_hlx(leadPho,subleadPho,leadJ,subleadJ):
-    pass
-
-
-## -----------------------------------------------------------------------------------------
-def cos_theta_cs(diPho,diJet):
-    pass
+#def cos_theta_hlx(leadPho,subleadPho,leadJ,subleadJ):
+#def cos_theta_cs(diPho,diJet):
